0334a5775b89a3b944d20d6e9a0ea03accc96dee
GinZeus <datnguyen2572003@gmail.com>
2023-06-01 19:30:07 +0700

    connect kotora be

--- src/shared/config/constants.ts
+++ src/shared/config/constants.ts
@@ -1,7 +1,7 @@
 // export const SERVER_API_URL = 'https://ekyc-api.solidtech.vn/caisd-rest/';
 // export const SERVER_API_URL = 'https://118.68.148.46:8443/CAisd/pdmweb.exe/';
 // export const SERVER_API_URL = 'https://hypersd.telehouse.com.vn:8050/caisd-rest/';
-export const SERVER_API_URL = 'https://telehouse.hypersd.vn:8050/caisd-rest/';
+export const SERVER_API_URL = 'http://192.168.1.237:8050/caisd-rest/';
 
 export const AUTH_USERNAME = 'telehouse';
 export const AUTH_PASSWORD = 'Tiv@2021#$';
